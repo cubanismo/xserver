e31f5e60db2efb074a7d5a20cfa135981ac2a6d0
James Jones <jajones@nvidia.com>
2010-05-11 11:32:15 -0700

    Fix (c) symbol in misync files.

--- miext/X/misync.c
+++ miext/X/misync.c
@@ -1,5 +1,5 @@
 /*
- * Copyright Â©2010 NVIDIA
+ * Copyright © 2010 NVIDIA
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
